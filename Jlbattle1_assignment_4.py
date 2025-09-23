8ae6dcba6382bd6462adb4c09b5fc54838721020
jerryekalbattle <jerryekalbattle@gmail.com>
2025-09-23 15:05:28 -0400

    Add files via upload
    "Pass Test Case 2: Course planning with if/elif/else and comparison operators"

--- Jlbattle1_assignment_4.py
+++ Jlbattle1_assignment_4.py
@@ -5,3 +5,46 @@ study_hours = 25
 social_points = 50
 stress_level = 80
 print(f"Welcome to College Life Adventure Game\n\nYour Stats\n\tName: {student_name}\n\tGPA: {current_gpa}\n\tStudy Hours: {study_hours}\n\tSocial Points {social_points}\n\tStress Level {stress_level}")
+
+# Step 2: Course Planning Decision
+print("Choose your Entertainment Time:")
+print("A) 1 Hours per day")  
+print("B) 4 Hours per day")
+print("C) 7 Hours per day")
+
+choice = input("Your Choice: ")
+if choice == "A":
+    if current_gpa <= 2.8:
+        print("You Failed")
+    else:
+        study_hours = study_hours + 5
+        stress_level = stress_level - 10
+        if stress_level >= 100:
+            print("You've Died")
+        else:
+            if study_hours <= 0:
+                print("You've Died")
+                if stress_level >= 100:
+                    print("You've Died")
+elif choice == "B":
+    if current_gpa <= 3.0:
+        print("You Failed")
+    else:
+        study_hours = study_hours - 5
+        stress_level = stress_level + 10
+        if stress_level >= 100:
+            print("You've Died")
+            if study_hours <= 0:
+                print("You've Died")
+elif choice == "C":
+    if current_gpa <= 3.5:
+        print("You Failed")
+    else:
+        study_hours = study_hours - 10
+        stress_level = stress_level + 20
+        if stress_level >= 100:
+            print("You've Died")
+            if study_hours <= 0:
+                print("You've Died")
+else:
+    print("Not Valid Try Again")
