ea17fe8584d4a3ee8700275b9b3165900fa53eab
jerryekalbattle <jerryekalbattle@gmail.com>
2025-09-23 14:17:28 -0400

    Add files via upload
    "Pass Test Case 1: Initial game setup with required variables"

---
+++ Jlbattle1_assignment_4.py
@@ -0,0 +1,7 @@
+# Step 1: Foundation Setup
+student_name = "Jerryeka Battle"
+current_gpa = 3.9
+study_hours = 25
+social_points = 50
+stress_level = 80
+print(f"Welcome to College Life Adventure Game\n\nYour Stats\n\tName: {student_name}\n\tGPA: {current_gpa}\n\tStudy Hours: {study_hours}\n\tSocial Points {social_points}\n\tStress Level {stress_level}")
